dd4dd1045983bb92c8bf856de08efd4a69127470
Pranay Kumar <39545459+chinnapranay123@users.noreply.github.com>
2021-07-27 12:05:02 +0530

    hi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,31 @@
 import { Component } from '@angular/core';
-import{ UsersService} from './users.service';
+import { UsersService } from './users.service';
 import * as $ from "jquery";
+import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  data:any;
-play:string="Play";
-videodisabled:boolean = true;
-  constructor(private postData:UsersService){}
-  ngOnInit()
-  {
-    this.postData.getPosts().subscribe((result)=>{
+  data: any;
+  play: string = "Play";
+  selectedVideo: any;
+  playableSelected: boolean = false;
+
+  constructor(private postData: UsersService, private sanitizer: DomSanitizer) { }
+
+  ngOnInit() {
+    this.postData.getPosts().subscribe((result) => {
       console.log(result);
-      this.data=result
+      this.data = result;
     })
 
   }
 
-
-
+  playVideoInEmbedFrame(videoURL: string) {
+    this.selectedVideo = this.sanitizer.bypassSecurityTrustResourceUrl(videoURL);
+    this.playableSelected = true;
+  }
 
 }
